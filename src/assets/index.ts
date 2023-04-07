0e43f42c2c4d1934406476a5ad20d347910870f0
JosephGakah <joegakah@gmail.com>
2023-04-07 13:50:59 +0300

    essai available for download

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -5,6 +5,8 @@ import tomnora from './img/tom-nora-ERSdstrilFA-unsplash.jpg';
 import write from './img/abhishek-tiwari-mvN4NJ560xQ-unsplash.jpg';
 import ceci from './img/ceci-li-oJC3bYdmn5Y-unsplash.jpg'
 import ian from './img/ian-talmacs-M-r5bhedn1U-unsplash.jpg'
+import prev1 from './img/ndcCLuu64mOM_1024_500.png';
+import prev2 from './img/gsJyErfpFb5k_1270_760.png';
 
 export {
     logoLight,
@@ -13,5 +15,7 @@ export {
     tomnora,
     write,
     ceci,
-    ian
+    ian,
+    prev1,
+    prev2
 }
